16cb6aee9d0bfe4b3aa8cf07b7a723e9f5e4b77b
Cornell van der Straaten <30183@ma-web.nl>
2020-04-08 19:26:09 +0200

    start myProjects

---
+++ script.js
@@ -0,0 +1,9 @@
+function testFunction(parameterA, parameterB) {
+    answer = parameterA  - parameterB;
+    console.log(answer);
+}
+
+let value1 = 10;
+let value2 = 5;
+
+testFunction(value1, value2);
